c17210f33e3c7a877ff6c562c10d4261ca162d21
seb-art <kimanzikims300@gmail.com>
2023-06-07 21:36:46 +0300

    added a like button

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -7,36 +7,49 @@ class Movies extends Component {
   };
 
   handleDelete = (movie) => {
-    const movies = this.state.movies.filter(m => m._id !== movie._id);
+    const movies = this.state.movies.filter((m) => m._id !== movie._id);
     this.setState({ movies });
-
-  }
+  };
   render() {
+    const { length: count } = this.state.movies;
+    if (count === 0) return <p>No Movies Found</p>;
     return (
-      <table className="table">
-        <thead>
-          <tr>
-            <th>Title</th>
-            <th>Genre</th>
-            <th>Stock</th>
-            <th>Rate</th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>
-          {this.state.movies.map((movie) => {
-            return (
-              <tr key = {movie._id}>
-                <td>{movie.title}</td>
-                <td>{movie.genre.name}</td>
-                <td>{movie.numberInStock}</td>
-                <td>{movie.dailyRentalRate}</td>
-                <td><button onClick={() => this.handleDelete(movie)}  className="btn btn-danger btn-sm">Delete</button></td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+      <>
+        <p>Showing {count} movies available</p>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Genre</th>
+              <th>Stock</th>
+              <th>Rate</th>
+              <th></th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.movies.map((movie) => {
+              return (
+                <tr key={movie._id}>
+                  <td>{movie.title}</td>
+                  <td>{movie.genre.name}</td>
+                  <td>{movie.numberInStock}</td>
+                  <td>{movie.dailyRentalRate}</td>
+                  <td><i class="fa fa-heart-o" aria-hidden="true"></i></td>
+                  <td>
+                    <button
+                      onClick={() => this.handleDelete(movie)}
+                      className="btn btn-danger btn-sm"
+                    >
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </>
     );
   }
 }
