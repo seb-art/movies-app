34f960f6a8666cc94ac0ffea44bdd800d6b132de
seb-art <kimanzikims300@gmail.com>
2023-06-16 00:25:43 +0300

    pagination activated

--- src/components/common/pagination.jsx
+++ src/components/common/pagination.jsx
@@ -1,16 +1,22 @@
 import React from "react";
 import _ from "lodash";
 
-const Pagination = () => {
-  const { itemsCount, pageSize } = props;
-  const pagesCount = itemsCount / pageSize;
+const Pagination = (props) => {
+  const { itemsCount, pageSize, currentPage, onPageChange } = props;
+  const pagesCount = Math.ceil(itemsCount / pageSize);
+  if (pagesCount === 1) return null;
   const pages = _.range(1, pagesCount + 1);
   return (
     <nav aria-label="Page navigation example">
       <ul className="pagination">
         {pages.map((page) => (
-          <li key={page} className="page-item">
-            <a class="page-link">{page}</a>
+          <li
+            key={page}
+            className={page === currentPage ? "page-item active" : "page-item"}
+          >
+            <a class="page-link" onClick={() => onPageChange(page)}>
+              {page}
+            </a>
           </li>
         ))}
       </ul>
--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -6,6 +6,7 @@ import Pagination from "./common/pagination";
 class Movies extends Component {
   state = {
     movies: getMovies(),
+    currentPage: 1,
     pageSize: 4,
   };
 
@@ -21,9 +22,13 @@ class Movies extends Component {
     movies[index].liked = !movies[index].liked;
     this.setState({ movies });
   };
-  handlePageChange = (page) => {};
+  handlePageChange = (page) => {
+    this.setState( {currentPage:page})
+  };
   render() {
     const { length: count } = this.state.movies;
+    const { pageSize, currentPage } = this.state;
+
     if (count === 0) return <p>No Movies Found</p>;
     return (
       <>
@@ -61,19 +66,20 @@ class Movies extends Component {
                       Delete
                     </button>
                   </td>
-                </tr> 
+                </tr>  
               );
             })}
           </tbody>
         </table>
         <Pagination
           itemsCount={count}
-          pageSize={this.state.pageSize}
+          pageSize={pageSize}
+          currentPage={currentPage}
           onPageChange={this.handlePageChange}
         />
       </>
     );
   }
 }
-
+ 
 export default Movies;
