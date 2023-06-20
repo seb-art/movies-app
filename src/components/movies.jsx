95da42455329d3042e30838223c023fb6a67b252
seb-art <kimanzikims300@gmail.com>
2023-06-20 11:03:27 +0300

    implemented sorting in asc,desc

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -5,16 +5,18 @@ import Pagination from "./common/pagination";
 import { paginate } from "../utils/paginate";
 import { getGenres } from "../services/fakeGenreService";
 import MoviesTable from "./moviesTable";
+import _ from 'lodash';
 
 class Movies extends Component {
   state = {
     movies: [],
     currentPage: 1,
-    pageSize: 5,
+    pageSize: 4,
     genres: [],
+    sortColumn: { path: 'title', order: 'asc'}
   };
   componentDidMount() {
-    const genres = [{ name: "All Genres" }, ...getGenres()];
+    const genres = [{ _id: "",name: "All Genres" }, ...getGenres()];
     this.setState({ movies: getMovies(), genres });
   }
 
@@ -36,12 +38,16 @@ class Movies extends Component {
   handleGenreSelect = (genre) => {
     this.setState({ selectedGenre: genre, currentPage: 1 });
   };
+  handleSort = sortColumn => {
+    this.setState({ sortColumn })
+  }
 
   render() {
     const { length: count } = this.state.movies;
     const {
       pageSize,
       currentPage,
+      sortColumn,
       selectedGenre,
       movies: allMovies,
     } = this.state;
@@ -52,7 +58,10 @@ class Movies extends Component {
       selectedGenre && selectedGenre._id
         ? allMovies.filter((m) => m.genre._id === selectedGenre._id)
         : allMovies;
-    const movies = paginate(filtered, currentPage, pageSize);
+
+    const sorted = _.orderBy(filtered, [sortColumn.path], [sortColumn.order]);
+
+    const movies = paginate(sorted, currentPage, pageSize);
     return (
       <div className="row">
         <div className="col-3">
@@ -66,8 +75,10 @@ class Movies extends Component {
           <p>Showing {filtered.length} movies available</p>
           <MoviesTable
             movies={movies}
+            sortColumn={sortColumn}
             onLike={this.handleLike}
             onDelete={this.handleDelete}
+            onSort={this.handleSort}
           />
 
           <Pagination
