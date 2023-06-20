3643eaa557521fc8c9250adab0664611c407ee0f
seb-art <kimanzikims300@gmail.com>
2023-06-20 14:03:30 +0300

    added unique keys

--- src/components/common/tableBody.jsx
+++ src/components/common/tableBody.jsx
@@ -6,16 +6,19 @@ class TableBody extends Component {
     if (column.content) return column.content(item);
     return _.get(item, column.path);
   };
-  state = {};
+
+  createKey = (item, column) => {
+    return item._id + (column.path || column.key)
+  }
   render() {
     const { data, columns } = this.props;
 
     return (
       <tbody>
         {data.map((item) => (
-          <tr>
+          <tr key={item._id}>
             {columns.map((column) => (
-              <td>{this.renderCell(item, column)}</td>
+              <td key={this.createKey(item, column)}>{this.renderCell(item, column)}</td>
             ))}
           </tr>
         ))}
--- src/components/moviesTable.jsx
+++ src/components/moviesTable.jsx
@@ -29,7 +29,7 @@ class MoviesTable extends Component {
   ];
 
   render() {
-    const { movies, onDelete, onLike, sortColumn, onSort } = this.props;
+    const { movies, sortColumn, onSort } = this.props;
     return (
       <table className="table">
         <TableHeader
