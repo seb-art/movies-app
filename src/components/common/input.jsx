70a041123911a8edb93d9ed6a01588e552a7947d
seb-art <kimanzikims300@gmail.com>
2023-06-24 15:06:15 +0300

    refactored existing code and extracted helper rendering methods

--- src/components/common/input.jsx
+++ src/components/common/input.jsx
@@ -1,18 +1,13 @@
-import React from 'react';
+import React from "react";
 
-const Input = ({name, label, value, error, onChange}) => {
-    return (<div className="form-group">
-    <label htmlFor={name}>{label}</label>
-    <input
-      value={value}
-      onChange={onChange}
-      id={name}
-      name={name}
-      type="text"
-      className="form-control"
-    />
-    {error && <div className="alert alert-danger">{error}</div>}
-  </div>);
-}
- 
+const Input = ({ name, label, error, ...rest }) => {
+  return (
+    <div className="form-group">
+      <label htmlFor={name}>{label}</label>
+      <input {...rest} name={name} id={name} className="form-control" />
+      {error && <div className="alert alert-danger">{error}</div>}
+    </div>
+  );
+};
+
 export default Input;
