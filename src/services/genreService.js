097b7f9c8810bba1fa448cf441b5c5b78bc8b26b
seb-art <kimanzikims300@gmail.com>
2023-07-18 18:26:43 +0300

    extracted the config file

--- src/services/genreService.js
+++ src/services/genreService.js
@@ -1,4 +1,5 @@
 import http from "./httpService";
+import { apiUrl } from "../config.json";
 export function getGenres() {
-  return http.get("http://localhost:3900/api/genres");
+  return http.get(apiUrl + "/genres");
 }
