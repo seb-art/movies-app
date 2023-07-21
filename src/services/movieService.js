8ce83c7084dae7ac2d8ddbca9afd3fb970c5849c
seb-art <kimanzikims300@gmail.com>
2023-07-21 13:48:43 +0300

    populating the form

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -6,6 +6,11 @@ const apiEndpoint = config.apiUrl + "/movies";
 export function getMovies() {
   return http.get(apiEndpoint);
 }
+export function getMovie(movieId) {
+  return http.get(apiEndpoint + "/" + movieId);
+}
+
+export function saveMovie(movie) {}
 
 export function deleteMovie(movieId) {
   return http.delete(apiEndpoint + "/" + movieId);
