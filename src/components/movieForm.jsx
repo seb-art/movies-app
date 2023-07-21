17f999b77d177a83f5b779e9aae7a28e3cb7f467
seb-art <kimanzikims300@gmail.com>
2023-07-21 14:06:43 +0300

    saving a movie to the DB

--- src/components/movieForm.jsx
+++ src/components/movieForm.jsx
@@ -63,8 +63,8 @@ class MovieForm extends Form {
     };
   }
 
-  doSubmit = () => {
-    saveMovie(this.state.data);
+  doSubmit = async () => {
+    await saveMovie(this.state.data);
 
     this.props.history.push("/movies");
   };
--- src/services/movieService.js
+++ src/services/movieService.js
@@ -10,7 +10,14 @@ export function getMovie(movieId) {
   return http.get(apiEndpoint + "/" + movieId);
 }
 
-export function saveMovie(movie) {}
+export function saveMovie(movie) {
+  if (movie._id) {
+    const body = { ...movie };
+    delete body._id;
+    return http.put(apiEndpoint + "/" + movie._id, body);
+  }
+  return http.post(apiEndpoint, movie);
+}
 
 export function deleteMovie(movieId) {
   return http.delete(apiEndpoint + "/" + movieId);
