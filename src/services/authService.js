d026d2ca0364e345d2f28862228e608373fe4694
seb-art <kimanzikims300@gmail.com>
2023-07-24 06:35:12 +0300

    called protected apiEndpoints from the backend

--- src/services/authService.js
+++ src/services/authService.js
@@ -5,6 +5,8 @@ import config from "../config.json";
 const apiEndpoint = config.apiUrl + "/auth";
 const tokenKey = "token";
 
+http.setJwt(getJwt());
+
 export async function login(email, password) {
   const { data: jwt } = await http.post(apiEndpoint, { email, password });
   localStorage.setItem(tokenKey, jwt);
@@ -27,9 +29,14 @@ export function getCurrentUser() {
   }
 }
 
+export function getJwt() {
+  localStorage.getItem(tokenKey);
+}
+
 export default {
   login,
   loginWithJwt,
   logout,
   getCurrentUser,
+  getJwt,
 };
